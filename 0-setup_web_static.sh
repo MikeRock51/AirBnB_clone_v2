e33c949b7c3ae619a6f147c8edd0522dc6b02cb5
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-08 19:04:16 +0100

    Add autoindex on; to task 0 script ; Low battery

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,6 @@ sudo touch /data/web_static/releases/test/index.html
 echo "Hello Mike Rock, I'm Obidient and working" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-replacement="server_name _;\n\n\tlocation \/hbnb_static\/ {\n\t\t alias \/data\/web_static\/current\/;\n\t\tadd_header X-Served-By \$hostname;\n\t}"
+replacement="server_name _;\n\n\tlocation \/hbnb_static\/ {\n\t\t alias \/data\/web_static\/current\/;\n\t\tautoindex on;\n\t\tadd_header X-Served-By \$hostname;\n\t}"
 sudo sed -i "s/server_name _;/$replacement/" /etc/nginx/sites-enabled/default
 sudo service nginx restart
