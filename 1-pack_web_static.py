6b5d5922beadcefe9b4788f0fc52313da0e7de44
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-08 08:30:17 +0100

    Update archive path ; Low Battery

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,22 @@ def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static
     """
+
+    # Create versions directory if not exist
     local("mkdir -p versions")
-    path = "versions/web_static_$(date +'%Y%m%d%H%M%S').tgz"
+
+    # Create the date format string
+    date_process = subprocess.run("date +'%Y%m%d%H%M%S'", shell=True,
+            capture_output=True, text=True)
+    if data_process.returncode == 0:
+        date_string = date_process.strip()
+    else:
+        return None
+
+    # Set the archive path
+    path = "versions/web_static_{}.tgz".format(date_string)
+
+    # Create archive
     status = local("tar -cvzf {} ./web_static/".format(path))
 
     if status.succeeded:
