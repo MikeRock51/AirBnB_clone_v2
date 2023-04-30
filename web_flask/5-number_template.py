6118c78dd445f6996d725e2ca1864e30facc0f2d
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-30 07:38:05 +0100

    Add more docs Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Returns a page"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Returns a page"""
     return "HBNB"
 
 
