d54e68c58aadc81be1dd09e2f9541d857f69e724
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-30 10:43:39 +0100

    Add init method to City

--- models/state.py
+++ models/state.py
@@ -11,8 +11,9 @@ from os import getenv
 
 class State(BaseModel, Base):
     """The state class"""
+    __tablename__ = 'states'
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade="all, delete", backref="state")
     else:
