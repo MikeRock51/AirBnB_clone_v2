3f8df3d32d5fc37358dc28b9df0306bf7b44d98e
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-01 08:48:33 +0100

    Add Task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Starts a flask web application on 0.0.0.0:5000"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tearDown(self):
+    """Removes the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def fetchState():
+    """Fetches all states instances from storage"""
+    allStates = storage.all(State)
+    return render_template('7-states_list.html', states=allStates)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
